8460ada474cc8470f17954fcecafb276ab2bf214
Damien <dvilleneuve@excilys.com>
2013-04-11 20:38:00 +0200

    Add buttons in sample project to change month

--- sample/src/main/java/fr/days/calendarview/sample/MainActivity.java
+++ sample/src/main/java/fr/days/calendarview/sample/MainActivity.java
@@ -2,13 +2,14 @@ package fr.days.calendarview.sample;
 
 import org.joda.time.YearMonth;
 
+import android.app.Activity;
+
 import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.ViewById;
 
 import fr.days.calendarview.CalendarView;
-
-import android.app.Activity;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
@@ -16,9 +17,24 @@ public class MainActivity extends Activity {
 	@ViewById
 	CalendarView calendarView;
 
+	private YearMonth currentMonth;
+
 	@AfterViews
 	void init() {
-		calendarView.setCurrentMonth(YearMonth.now());
+		currentMonth = YearMonth.now();
+		calendarView.setCurrentMonth(currentMonth);
 	}
-	
+
+	@Click
+	void previousButtonClicked() {
+		currentMonth = currentMonth.minusMonths(1);
+		calendarView.setCurrentMonth(currentMonth);
+	}
+
+	@Click
+	void nextButtonClicked() {
+		currentMonth = currentMonth.plusMonths(1);
+		calendarView.setCurrentMonth(currentMonth);
+	}
+
 }
