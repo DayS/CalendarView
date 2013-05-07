497d4527055b0799bbe0d390ad23357d7d82c467
Damien <dvilleneuve@excilys.com>
2013-05-07 20:01:02 +0200

    Add javadoc on Month class

--- library/src/main/java/fr/days/calendarview/Month.java
+++ library/src/main/java/fr/days/calendarview/Month.java
@@ -6,6 +6,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+/**
+ * This class represent a month for a given year. It offers some methods to easily retrieve the first day of the month and wich day of week is this day.
+ */
 public class Month implements Serializable {
 
 	private static final long serialVersionUID = -7678210275754915223L;
@@ -13,14 +16,28 @@ public class Month implements Serializable {
 	private final Calendar date;
 	private SimpleDateFormat monthDateFormat;
 
+	/**
+	 * Create a new {@link Month} for the current month and year
+	 */
 	public Month() {
 		this(-1);
 	}
 
+	/**
+	 * Create a new {@link Month} for the the given month and current year
+	 * 
+	 * @param month
+	 */
 	public Month(int month) {
 		this(-1, month);
 	}
 
+	/**
+	 * Create a new {@link Month} for the given month and year
+	 * 
+	 * @param year
+	 * @param month
+	 */
 	public Month(int year, int month) {
 		date = Calendar.getInstance();
 		date.set(Calendar.DAY_OF_MONTH, 1);
@@ -43,30 +60,57 @@ public class Month implements Serializable {
 		return date.get(Calendar.MONTH) + 1;
 	}
 
-	public int getFirstDayOfFirstWeek() {
-		return date.get(Calendar.DAY_OF_WEEK);
-	}
-
+	/**
+	 * Return the actual number of days in this month. This handle odd year.
+	 */
 	public int getNumberOfDays() {
 		return date.getActualMaximum(Calendar.DAY_OF_MONTH);
 	}
 
+	/**
+	 * Return the week day of the first week of this month.
+	 * 
+	 * @return return a value between {@link Calendar#MONDAY} and {@link Calendar#SUNDAY}
+	 */
+	public int getFirstDayOfFirstWeek() {
+		return date.get(Calendar.DAY_OF_WEEK);
+	}
+
+	/**
+	 * Return the first day of this month.
+	 */
 	public Calendar getFirstDayOfMonth() {
 		Calendar firstDate = (Calendar) date.clone();
 		firstDate.set(Calendar.DAY_OF_MONTH, 1);
 		return firstDate;
 	}
 
+	/**
+	 * Check if the given date is on this month (ie: day is in the range of this month and the month is equals to this one)
+	 * 
+	 * @param date
+	 * @return <code>true</code> if the date is in this month, <code>false</code> elsewhere
+	 * @see Month#contains(Calendar)
+	 */
 	public boolean contains(Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return contains(cal);
 	}
 
+	/**
+	 * Check if the given date is on this month (ie: day is in the range of this month and the month is equals to this one)
+	 * 
+	 * @param date
+	 * @return <code>true</code> if the date is in this month, <code>false</code> elsewhere
+	 */
 	public boolean contains(Calendar date) {
 		return date.get(Calendar.YEAR) == this.date.get(Calendar.YEAR) && date.get(Calendar.MONTH) == this.date.get(Calendar.MONTH);
 	}
 
+	/**
+	 * Return the current month's full name for the current {@link Locale}.
+	 */
 	public String getMonthName() {
 		if (monthDateFormat == null) {
 			monthDateFormat = new SimpleDateFormat("MMM", Locale.getDefault());
@@ -74,6 +118,9 @@ public class Month implements Serializable {
 		return monthDateFormat.format(date.getTime());
 	}
 
+	/**
+	 * Return a new instance of {@link Month} which is the previous month. If current month is {@link Calendar#JANUARY}, the new instance will be {@link Calendar#DECEMBER}
+	 */
 	public Month previousMonth() {
 		int month = getMonth();
 		if (month == 1) {
@@ -83,6 +130,9 @@ public class Month implements Serializable {
 		}
 	}
 
+	/**
+	 * Return a new instance of {@link Month} which is the next month. If current month is {@link Calendar#DECEMBER}, the new instance will be {@link Calendar#JANUARY}
+	 */
 	public Month nextMonth() {
 		int month = getMonth();
 		if (month == 12) {
